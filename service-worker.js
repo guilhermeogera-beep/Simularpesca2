9d150f58f5cd417d9f8be418879ba66b0acbd1e4
guilhermeogera-beep <guilhermeogera@gmail.com>
2026-05-16 01:16:10 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,3 @@
-const CACHE_NAME = 'capacete-seco-v4'; 
-const assets = ['./index.html', './manifest.json', './icon.svg'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
+self.addEventListener('install', (e) => { self.skipWaiting(); });
+self.addEventListener('activate', (e) => { e.waitUntil(self.clients.claim()); });
+self.addEventListener('fetch', (e) => { e.respondWith(fetch(e.request).catch(() => {}); });
